6fce8400774fcfce321c9546e1b86c6b08f3becf
Nikolai <nlv.nki@gmail.com>
2021-10-30 13:35:20 +0300

    sticky was implemented

--- additional.js
+++ additional.js
@@ -1,18 +1,24 @@
 'use strict';
-const genConrols = () => {
+ const genConrolsBtn = () => {
  const btnWrap = document.querySelectorAll('.button');
  const button = document.querySelectorAll('.btn-success');
- const link = 
  button.forEach(item => {
-    item.innerHTML = 'Добавить в абонемент'
-    item.classList.add('btn-add')
+    item.innerHTML = 'Добавить в абонемент';
+    item.classList.add('btn-add');
     item.style.background = 'linear-gradient(to right, #095bd6, #69b0f6)';
-    item.style.borderRadius = '15px'
- })
+    item.style.borderRadius = '15px';
+ });
 
- btnWrap.forEach(item => item.insertAdjacentHTML('beforeend', '<a href="#" class="btn-link">Подробнее</a>'))
+ btnWrap.forEach(item => item.insertAdjacentHTML('beforeend', '<a href="#" class="btn-link">Подробнее</a>'));
 
-}
+};
+
+const addSticky = () => {
+   const imageIcard = document.querySelectorAll('.image-card');
+   console.log(imageIcard, 111111111111111);
+   imageIcard.forEach(item => item.insertAdjacentHTML('beforeend', '<div class="round-sticky">NEW</div>'));
+};
 
 
-genConrols();
+genConrolsBtn();
+addSticky();
